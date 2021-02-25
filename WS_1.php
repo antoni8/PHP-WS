5b1a2bb1d7db13ba6fb9c1989201cd8ea9693305
Antoni <inotna.21@gmail.com>
2021-02-25 20:02:00 +0100

    WS_1

--- WS_1.php
+++ WS_1.php
@@ -53,4 +53,23 @@ echo $jmperezperez_json = json_encode($jmperezperez);
 echo "<br>JSON format array<br>";
 echo var_dump($jmperezperez_json)."<br>";
 echo print_r($jmperezperez_json)."<br>";
+
+$jmperezperez_json_decode = json_decode($jmperezperez_json);
+echo "<br>JSON decode<br>";
+echo var_dump($jmperezperez_json_decode)."<br>";
+echo print_r($jmperezperez_json_decode)."<br>";
+
+$elements_3='{
+  "jmperezperez": true,
+  "thelinmichael": false,
+  "wizzler": false
+}';
+echo "3 elements string format<br>";
+echo var_dump($elements_3)."<br>";
+echo print_r($elements_3)."<br>";
+
+$result_arr_ass=json_decode($elements_3, true);
+echo "jmperezperez".$result_arr_ass["jmperezperez"];
+echo "thelinmichael".$result_arr_ass["thelinmichael"];
+echo "wizzler".$result_arr_ass["wizzler"];
 ?>
